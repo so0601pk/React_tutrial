9020ee68cbfb22ba3e15b2cada5d45bbb973d819
so0601pk <youhami9@gmail.com>
2021-10-10 14:56:30 +0900

    公開ボタンを押下すると表示が変わる機能を作成

--- src/component/Article.jsx
+++ src/component/Article.jsx
@@ -1,10 +1,17 @@
 import {Content, Title} from "./index";
+import { useState } from "react";
+import PublishedButton from "./PublishedButton";
 
 const Article = (props) => {
+    const [isPublished, setIsPublished] = useState(false)
+    const publishedArticle = () =>{
+        setIsPublished(true)
+    }
     return (
         <div>
             <Title title={props.title} />
             <Content content={props.content} />
+            <PublishedButton isPublished={isPublished} onClick={publishedArticle} />
         </div>
     );
 };
