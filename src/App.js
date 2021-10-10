b7c2d102bccc06a35576d2188b4156363906eb61
so0601pk <youhami9@gmail.com>
2021-10-10 12:52:04 +0900

    conponentについて学習

--- src/App.js
+++ src/App.js
@@ -7,14 +7,6 @@ function App(){
         title={'新・日本一わかりやすいReact入門基礎編4'}
         content={'今日のトピックスはpropsについて'}
       />
-      <Article
-        title={'新・日本一わかりやすいReact入門基礎編5'}
-        content={'今日のトピックスはuseStateの受け渡しについて'}
-      />
-      <Article
-        title={'新・日本一わかりやすいReact入門基礎編6'}
-        content={'今日のトピックスはuseEffectについて'}
-      />
     </div>
   );
 }
--- src/component/Title.jsx
+++ src/component/Title.jsx
@@ -1,5 +1,11 @@
 const Title = (props) => {
-    return <h2>{props.title}</h2>
+    return (
+        // <h2>{props.title}</h2>
+        <div className='flex__row'>
+            <img src={'/logo192.png'} width={36} height={36} />
+            <h2>{props.title}</h2>
+        </div>
+    );
 };
 
 export default Title;
